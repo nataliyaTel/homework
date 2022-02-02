538a8e4f747d339ed2629695978f83e190cd6b94
nataliyaTel <septem79@mail.ru>
2022-02-02 12:57:18 +0300

    homeworkn4

---
+++ homeworkn4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Найти максимальное из трех чисел");
+
+ int a =80;
+ int b =2; 
+ int k =9;
+ int max = a;
+if (a>max) max = a;
+if (b>max) max = b;
+if (k>max) max = k;
+    
+Console.Write(" max = ");
+Console.WriteLine(max);
