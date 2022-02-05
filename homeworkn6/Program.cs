55b04e9bdcee9877e8f25a909d4edf38aee162db
nataliyaTel <septem79@mail.ru>
2022-02-05 17:52:50 +0300

    homeworkn6

--- homeworkn6/Program.cs
+++ homeworkn6/Program.cs
@@ -1,8 +1,8 @@
 ﻿
 Console.WriteLine("Выяснить, является ли число  x чётным  ");
 
-int x = 11;
+int x = 1222059;
 if ( x % 2 == 0) 
 Console.WriteLine("x - чётное число ");
-if (x % 2 > 0 )
+else
 Console.WriteLine("x - нечётное число ");
