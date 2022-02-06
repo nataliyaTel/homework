92adc3d0519f5b7a6df6ad622fa04a1b8dd6454b
nataliyaTel <septem79@mail.ru>
2022-02-06 23:34:55 +0300

    homeworkn10

---
+++ homeworkn10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int Numer(int k)
+{
+ int num =k / 10;
+ return num ;
+}    
+int Numer1(int b)
+{
+ int num1 =b % 10;
+ return num1 ;
+}   
+Console.WriteLine("Ввести трёхзначное число - ");
+int a = int.Parse(Console.ReadLine());
+int k =Numer(a);
+int b =Numer1(k);
+Console.WriteLine( "вторая цифра : " + b);
