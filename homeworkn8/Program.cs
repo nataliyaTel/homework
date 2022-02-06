815ceb56141d90def1d3f9797be014fd885cb12f
nataliyaTel <septem79@mail.ru>
2022-02-06 17:43:06 +0300

    homeworkn8

---
+++ homeworkn8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать чётные числа от 1 до N
+Console.WriteLine("");
+int a = 1;
+Console.WriteLine("введите число"); 
+int b =int.Parse(Console.ReadLine()) ;
+ 
+   for (int i =a; i<=b; i++)
+   { 
+     if ( i %2 == 0)
+     {    
+      Console.WriteLine( i + " ") ;
+     }
+   }
+
+ 
+
+
+
