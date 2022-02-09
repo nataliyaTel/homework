04828dc03f3e6a9603e50edb4abd905748a13c68
nataliyaTel <septem79@mail.ru>
2022-02-09 23:51:46 +0300

    homeworkn12

---
+++ homeworkn12/Program.cs
@@ -0,0 +1,10 @@
+﻿// Удалить вторую цифру трёхзначного чиcла
+  
+Console.WriteLine("Введите трёхзначное число ");
+int a = int.Parse(Console.ReadLine());
+int num3 = a % 10;
+int num1 = a / 100;
+Console.Write( num1);
+Console.Write( num3);
+
+
