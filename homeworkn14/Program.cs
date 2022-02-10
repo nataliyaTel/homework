78d2b4941af88d401f6bad7c0d64d79c2ca63d6a
nataliyaTel <septem79@mail.ru>
2022-02-10 21:24:03 +0300

    homeworkn14

---
+++ homeworkn14/Program.cs
@@ -0,0 +1,42 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int num = InputNumber();
+
+        string str = num.ToString();
+       try
+       {
+            Console.WriteLine(str[2]);
+        }
+       catch
+        {
+           Console.WriteLine($"Третьей цифры нет. Число {str.Length}-значное.");
+        }
+
+    }
+
+private static int InputNumber()
+{
+    while (true)
+    {
+        Console.WriteLine("Введите число: ");
+        string Str1 = Console.ReadLine();
+
+        bool i = int.TryParse(Str1, out int chek1);
+
+        if (!i)
+        
+            Console.WriteLine("Вы ввели не число. Попробуйте ещё раз");
+        
+        else 
+        
+            return chek1;
+        }
+    }
+}
+
+        
+    
+
