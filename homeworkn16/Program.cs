6e7842b63b5a2383a5ebe0934bb0b5ed0a37c0cc
nataliyaTel <septem79@mail.ru>
2022-02-14 20:07:55 +0300

    homeworkn16

---
+++ homeworkn16/Program.cs
@@ -0,0 +1,27 @@
+﻿// Дано число обозначающее день недели.Выяснить,является номер дня недели выходным днём.
+Console.WriteLine(" Введите число от 1 до 7");
+int a = int.Parse(Console.ReadLine());
+
+  while (true)
+  {
+     if (  a ==1 || a== 2 || a== 3 || a==4 || a ==5)
+      { 
+        
+         Console.WriteLine("будний день" );
+  
+      }
+      else if (  a==6 || a ==7 )
+      {
+
+         Console.WriteLine("выходной день");
+      
+      }
+   return;
+  }   
+      
+      
+  
+    
+
+            
+    
