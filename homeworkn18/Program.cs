53332f2da46d4c4b25596b507ed90f99143a5d3e
nataliyaTel <septem79@mail.ru>
2022-02-17 23:58:02 +0300

    homeworkn18

---
+++ homeworkn18/Program.cs
@@ -0,0 +1,17 @@
+﻿// Проверить истинность утверждения не (X v Y)= не X ^ не Y
+ 
+
+foreach (bool x in new[] { false, true })
+            {
+                foreach (bool y in new[] { false, true })
+                {
+                    if (!(x | y) == (!x & !y))
+                    {
+                        Console.WriteLine("true");
+                    }
+                    else
+                    {
+                        Console.WriteLine("false");
+                    }
+                }
+            }
