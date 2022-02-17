14c67f5a693ad2c4851084a3f822fd6f611c5548
nataliyaTel <septem79@mail.ru>
2022-02-18 00:00:27 +0300

    homeworkn24

---
+++ homeworkn24/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.WriteLine("введите число"); 
+int a =1 ;
+int b =int.Parse(Console.ReadLine());
+
+   for (int i =a; i<=b; i++)
+   { 
+             
+      Console.WriteLine( i*i*i) ;
+    
+   }
+
+ 
