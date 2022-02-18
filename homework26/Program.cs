373ded0033f16f64ca14cee0273e4ecc3995d20a
nataliyaTel <septem79@mail.ru>
2022-02-18 20:14:40 +0300

    homework26

---
+++ homework26/Program.cs
@@ -0,0 +1,24 @@
+﻿// Возведите число А в натуральную степерь В,используя цикл.
+
+Console.WriteLine("Введите число А- основание степени: ");
+
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число В -показатель степени : ");
+
+int B = int.Parse(Console.ReadLine());
+
+int erection = A;
+
+for (int i = 1; B > i; i++) erection = erection * A;
+
+Console.WriteLine($"Возведение числа {A} в степень {B} = {erection}");
+
+
+
+
+
+
+
+
+
