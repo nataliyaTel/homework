debd126a33821295987d765abe5876a5dea653bc
nataliyaTel <septem79@mail.ru>
2022-02-21 22:51:58 +0300

    homework28

---
+++ homework28/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе .
+      
+     Console.WriteLine(" Введите число");
+     int n=int.Parse(Console.ReadLine());             
+     int sum = 0;
+       
+       while (n > 0) 
+       {
+         sum = sum + n % 10;
+         n = n / 10;
+       }
+           
+     Console.WriteLine($"Сумма цифр в числе равна  = {sum}");
+    
+    
