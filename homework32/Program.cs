41200d99d69828c14ed438f4427aebf7ee523492
nataliyaTel <septem79@mail.ru>
2022-02-22 00:34:34 +0300

    homework32

---
+++ homework32/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задайте массив из восьми элементов ,заполненных нулями и единицами.Выведите их на экран.
+
+int[] array = {0,1,0,1,0,1,0,1};
+
+void Function(int[]array)
+{
+     for (int i =0; i< array.Length; i++) 
+     Console.WriteLine(array[i]);
+}
+Function(array);
