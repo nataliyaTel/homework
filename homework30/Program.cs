900f4b6bdc463ee0fd6fabd52dc56c457018d869
nataliyaTel <septem79@mail.ru>
2022-02-21 23:32:11 +0300

    homework30

---
+++ homework30/Program.cs
@@ -0,0 +1,14 @@
+﻿// Покажите кубы чисел ,заканчивающися на чётную цифру .
+
+Console.WriteLine("Введите число"); 
+int a =1 ;
+int b =int.Parse(Console.ReadLine());
+ 
+   for (int i =a; i<=b; i++)
+   { 
+       if ( i*i*i %2 == 0)
+     {    
+      Console.WriteLine( i*i*i ) ;
+     }      
+      
+   } 
