c390e6e00ddf3fc1ed01aeaa30c4d312a10c8a45
nataliyaTel <septem79@mail.ru>
2022-02-25 00:44:35 +0300

    homework36

---
+++ homework36/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задать массив,заполнить случайными положительными трёхзначными числами.Показать количество нечётных/четных чисел.
+
+void Function(int[] array)
+{
+    int length = array.Length;
+    int i = 0;
+    for (i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int i = 0;
+    for (i= 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void QuanChetOdd(int[] array)
+{
+    int count = array.Length;
+    int i = 0;
+    int quanChet = 0;
+    int quanOdd = 0;
+
+    for (i = 0; i < count; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            quanChet++;
+        }
+        else
+        {
+            quanOdd++;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Количество чётных чисел в массиве  = {quanChet}");
+    Console.WriteLine();
+    Console.WriteLine($"Количество нечётных чисел в массиве = {quanOdd}");
+}
+int[] chetOdd = new int[9];
+Console.WriteLine();
+Function(chetOdd);
+PrintArray(chetOdd);
+Console.WriteLine();
+QuanChetOdd(chetOdd);
+Console.WriteLine();
