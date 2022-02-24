e3b75af03363ba809d13eab3d07830d3de61288a
nataliyaTel <septem79@mail.ru>
2022-02-25 00:16:07 +0300

    homework34

---
+++ homework34/Program.cs
@@ -0,0 +1,43 @@
+﻿// Написать программу для замены элементов массива на противоположные.
+
+
+void Function(int[] array)
+{
+    int length = array.Length;
+    int i = 0;
+    for (i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int i = 0;
+    for (i= 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+
+void OppositeArray(int[] array)
+{
+    int count = array.Length;
+    int i = 0;
+    for (i = 0; i < count; i++)
+    {
+        array[i] = array[i] * -1;
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] opposite = new int[20];
+
+Function(opposite);
+PrintArray(opposite);
+Console.WriteLine();
+OppositeArray(opposite);
+Console.WriteLine();
