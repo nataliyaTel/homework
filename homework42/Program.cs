843d429c9043ad9dad4365ffac356cbc09724f9a
nataliyaTel <septem79@mail.ru>
2022-03-09 20:29:03 +0300

    homework42

---
+++ homework42/Program.cs
@@ -0,0 +1,15 @@
+﻿// Определить, сколько чисел больше 0 введено с клавиатуры.
+
+ Console.WriteLine("Введите значение N - количество чисел вводимых с клавиатуры ");
+ int n = Convert.ToInt32( Console.ReadLine() );
+ int col = 0;
+ for (int i=0;i<n;i++)
+  {
+    Console.WriteLine("Введите число: "); 
+    int a = Convert.ToInt32( Console.ReadLine() );
+    if (a>0) col = col + 1 ;
+    
+  }  
+ Console.WriteLine("количество положительных чисел = {0}", col);
+ Console.ReadKey();
+
