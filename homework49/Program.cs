26c1ef9d04eeadefbb2f93ad59ada0255bd2965b
nataliyaTel <septem79@mail.ru>
2022-03-11 21:35:08 +0300

    homework49

---
+++ homework49/Program.cs
@@ -0,0 +1,36 @@
+﻿// Показать двумерный массив размером m×n, заполненный вещественными числами.
+
+
+
+ double [,] matrix = new double [4,3];
+
+ void PrintArray(double[,]matr)
+ {
+ 
+   for(int i=0; i< matr.GetLength(0); i++)
+    {
+      for(int j=0; j<matr.GetLength(1); j++)
+       {
+          Console.Write( $"{ matr [i,j] } ");
+       }
+      Console.WriteLine();
+    }
+ }
+ 
+ void FillArray(double[,] matr)
+  {
+
+    for(int i=0; i< matr.GetLength(0); i++)
+    {
+       for(int j=0; j<matr.GetLength(1); j++)
+       {
+           matr[i,j] = new Random().NextDouble();
+       }
+       
+    }
+
+  }
+ PrintArray( matrix);
+ FillArray( matrix);
+ Console.WriteLine();
+ PrintArray( matrix);
