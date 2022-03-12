3346fd5462786805147ec7097336f36d0e8e495e
nataliyaTel <septem79@mail.ru>
2022-03-12 09:42:21 +0300

    homework51

---
+++ homework51/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n.
+
+Console.WriteLine("Введите количество строк - m ");
+ int m =  int.Parse(Console.ReadLine());
+ Console.WriteLine("Введите количество строк - n ");
+ int n = int.Parse( Console.ReadLine());
+ int [,] arr = new int [m,n];
+
+    
+
+    
+    for(int i=0; i< arr.GetLength(0); i++)
+    {
+       for(int j=0; j<arr.GetLength(1); j++)
+       {
+           
+           arr[i,j] = m+n;
+           n++ ;
+           
+           Console.Write( $"{ arr [i,j] } ");
+       }
+      Console.WriteLine(); 
+    }
+
+    
+
+ 
+ 
+
+
+
+
+
+
+
